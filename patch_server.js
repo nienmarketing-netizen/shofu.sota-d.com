05053b53f12a5e02089f272c248af013cf138f4c
AI Studio Assistant <nien.marketing@gmail.com>
2026-08-26 16:05:33 +0000

    chore: cleanup temporary node scripts

--- patch_server.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('server.ts', 'utf8');
-content = content.replace(
-  'const webhookUrl = "https://script.google.com/macros/s/AKfycbxc-EtHL1Un2AgalFAz8RvxlHX0TtE4q6OK2h0CiSNWBo7tvP1sDhBiJv7vvrRkJ3-zgQ/exec";',
-  'const webhookUrl = "https://script.google.com/macros/library/d/1kKuAggz_Mc63pwdvBQWx8LZbWmk7Eq4KwXKVyX2JztmPDHpzHKdjnIFE/8";'
-);
-fs.writeFileSync('server.ts', content);
