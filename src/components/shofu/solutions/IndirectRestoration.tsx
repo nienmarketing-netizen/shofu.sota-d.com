d245b02c5bd6a431b3da171ab8dc79637ea75397
Nien Marketing <nien.marketing@gmail.com>
2026-08-28 10:05:27 +0000

    feat(ui): start autoplay video at 3 seconds to avoid youtube initial UI flash

--- src/components/shofu/solutions/IndirectRestoration.tsx
+++ src/components/shofu/solutions/IndirectRestoration.tsx
@@ -65,7 +65,7 @@ const YoutubeAutoplay = ({ videoId }: { videoId: string }) => {
         <>
           <iframe
             className="absolute top-0 left-0 w-full h-full pointer-events-none select-none"
-            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}&controls=0&disablekb=1&fs=0&modestbranding=1&playsinline=1&rel=0`}
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}&controls=0&disablekb=1&fs=0&modestbranding=1&playsinline=1&rel=0&start=3`}
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
